072e40e55c7c55b06002887087a17b268491cc94
isaeed <isaeed@asicentral.com>
2022-11-07 12:14:33 +0500

    first commit

---
+++ McqsUI/Controllers/WeatherController.cs
@@ -0,0 +1,66 @@
+﻿using McqsUI.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace McqsUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        // GET: api/<WeatherController>
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get(DateTime? startDate)
+        {
+            var rng = new Random();
+
+            if (startDate == null)
+            {
+                startDate = DateTime.Now;
+            }
+
+            var list = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.Value.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray();
+
+            return list;
+        }
+
+        // GET api/<WeatherController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<WeatherController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<WeatherController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<WeatherController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
